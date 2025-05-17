b652704ab03d3804d7b03677456993974db64613
OussamaMahjour <MahjourOussama@gmail.com>
2025-05-17 20:02:27 +0100

    user-service update

--- services/UserService/src/main/java/com/app/userservice/Mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.app.userservice.Mapper;
-
-import com.app.userservice.DTO.ResponseDto;
-import com.app.userservice.Entity.User;
-
-public interface UserMapper {
-   public ResponseDto userToResponseDto(User user);
-   public User responseDtoToUser(ResponseDto responseDto);
-}
