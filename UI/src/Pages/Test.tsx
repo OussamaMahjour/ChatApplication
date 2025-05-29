74ece949e41e3673531eeff397c5920c3d3f8e6b
OussamaMahjour <MahjourOussama@gmail.com>
2025-05-29 11:49:32 +0100

    test

--- UI/src/Pages/Test.tsx
+++ UI/src/Pages/Test.tsx
@@ -5,29 +5,45 @@ import Button from "../components/Button";
 import ButtonInverse from "../components/ButtonInverse";
 import ThemeButton from "../components/ThemeButton";
 import { useTheme } from "../provider/ThemeProvider";
+import Input from "../components/Input";
 
 const Test = ():ReactNode=>{
 
     const {setAlert} = useTheme()
 
     return <div className="grid grid-flow-row gap-4  grid-cols-4 grid-rows-4  h-screen w-screen p-4 overflow-y-auto relative">
+            
             <Card className=" w-fit h-fit absolute bottom-3 right-0 ">
                 <ThemeButton />
              </Card> 
+            
             <Card className="p-2 w-full h-fit  ">
-                <FormInput placeholder="Form Input" id="test" type="text" className="w-full " />
+                <FormInput placeholder="Form Input" id="forminput" type="text" className="w-full " />
              </Card>
+            
              <Card className="p-2 w-full h-fit  ">
-                <FormInput placeholder="Form Input" id="test" type="text" className="w-full invalid " errorMessage="Invalid Form Input" />
+                <FormInput placeholder="Invalid Form Input" id="invalidforminput" type="text" className="w-full invalid " errorMessage="Invalid Form Input" />
              </Card>
+            
              <Card className="p-2 w-full h-fit">
                <Button className="w-full">
                     Button
                </Button>
              </Card>
+            
+             <Card className="p-2 w-full h-fit">
+               <ButtonInverse  className="w-full">
+                    ButtonInverse
+               </ButtonInverse>
+             </Card> 
+            
+            <Card className="p-2 w-full h-fit  ">
+                <Input placeholder="Normal Input" id="test" type="text" className="w-full invalid "  />
+            </Card>
+             
              <Card className="p-2 w-full h-fit">
                <ButtonInverse onClick={()=>setAlert({message:"This is an Alert message",timeout:1000})} className="w-full">
-                    ButtonInverse
+                    Click for alert
                </ButtonInverse>
              </Card> 
             
